b10dc960f30b8965abd23f0eb9986b347376d0ac
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2021-10-07 17:50:26 +0530

    some udpate

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
+set -e
 cd /home/ec2-user/node-server
-# set any env variables
-sudo pm2 delete demo
-sudo pm2 delete pm2-logrotate
-#pm2 start --name new_prod dist/server.js -o /var/log/new-prod-out.log  -e /var/log/new-prod-error.log
-pm2 start server.js --name demo
-#reload pm2 process
-pm2 reload demo
-#Restart logs rotate
-sudo systemctl restart awslogsd
-#pm2 LOGS ROTATE
-sudo pm2 install pm2-logrotate
-sudo pm2 set pm2-logrotate:max_size 100M
-sudo pm2 set pm2-logrotate:compress true
-sudo pm2 set pm2-logrotate:retain 50
-sudo pm2 set pm2-logrotate:dateFormat YYYY-MM-DD
-sudo pm2 set pm2-logrotate:rotateInterval '0 0   *'
+npm install
+npm run build
