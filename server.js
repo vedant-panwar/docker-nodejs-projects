a525482f8f3ee9c098cdb6ed6df03e6b8af7e60c
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2021-10-14 14:54:45 +0530

    version changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {PORT = 3000} = process.env;
 
 console.log('PORT',PORT);
 
-app.get('/',(req,res)=> res.send('Hello world !!!! V3'));
+app.get('/',(req,res)=> res.send('Hello world !!!! V1'));
 
 
 app.listen(PORT,()=>{
